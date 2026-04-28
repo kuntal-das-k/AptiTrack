0ff378a9da55211394e88fa4c17604e79eb02378
kuntal-das-k <daskuntal688@gmail.com>
2026-04-29 01:41:29 +0530

    Configure dj-database-url for Vercel deployment

--- aptitrack/settings.py
+++ aptitrack/settings.py
@@ -4,6 +4,7 @@ Django settings for AptiTrack project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,10 +62,10 @@ WSGI_APPLICATION = 'aptitrack.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 # Password validation
